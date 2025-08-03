11354ed5c740140907921a650c6c73a4c8c78630
Leon <codetips4572@gmail.com>
2025-08-03 12:43:12 +1000

    Added text input limits on login page

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -3,6 +3,8 @@ import { AlertContext } from "./Alert/AlertProvider"
 import { AiOutlineLoading } from "react-icons/ai";
 import { AuthContext } from "./Providers/AuthProvider";
 import { SignUpResponses } from "../utils/types";
+import { LuLogIn } from "react-icons/lu";
+import { Util } from "@/utils/util";
 
 interface FormProps{
     name: string
@@ -45,62 +47,82 @@ export default function Auth() {
 
     return (
         <div className="flex justify-center mt-17">
-            <div className="outline-1 outline-border bg-panel1 w-[90%] max-w-100 flex justify-center rounded-2xl p-5 flex-col items-center ">
-                <p className="text-title font-mono text-2xl m-1 mt-4 font-semibold">
+            <div className="outline-1 outline-border bg-panel1 w-[90%] max-w-100 flex justify-center rounded-md p-5 flex-col items-center ">
+                <p className="text-title  text-2xl m-1 mt-4 font-semibold">
                     {login ? "Log In" : "Sign Up"}
                 </p>
                 <div className="w-[90%] flex justify-center">
-                    <p className="text-subtext2 text-[13px] mb-7 font font-mono text-center">
+                    <p className="text-subtext2 mt-1 mb-5 font text-xs  text-center">
                     Start your improvement journey today
                 </p>
                 </div>
-                <form className=" w-[90%] flex flex-col gap-3 text-subtext1 font-mono" onSubmit={e => submit(e)}>
+                <form className=" w-[90%] flex flex-col gap-3 text-subtext1 " onSubmit={e => submit(e)}>
                     {!login ? <div>
-                        <p className="mb-1.5">Name</p>
+                        <p className="mb-1.5 text-sm font-medium">Name</p>
                         <input type="text" 
-                        onChange={(e) => setFormData(prev => ({...prev, name: e.target.value}))}
+                        onChange={(e) => Util.setValueLim((v: string) => setFormData(prev => ({...prev, name: v})), e.target.value, 25) }
                         value={formData.name}
                         placeholder="Enter name"
                         autoComplete="username"
-                        className="outline-1 text-[12px] rounded-xl w-full border-0 pl-2  outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        className="outline-1 text-[12px] rounded-sm w-full border-0 pl-2  outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        <div className="w-full flex justify-end mb-[-12px]">
+                            <p className="text-xs text-subtext3">
+                                {formData.name.length}/25
+                            </p>
+                        </div>
                     </div> : ""}
                     <div>
-                        <p className="mb-1.5">Email</p>
+                        <p className="mb-1.5 text-sm font-medium">Email</p>
                         <input type="text"  
-                        onChange={(e) => setFormData(prev => ({...prev, email: e.target.value}))}
+                        onChange={(e) => Util.setValueLim((v: string) => setFormData(prev => ({...prev, email: v})), e.target.value, 70)}
                         value={formData.email}
                         placeholder="Enter email"
                         autoComplete="email"
-                        className="outline-1 text-[12px] rounded-xl w-full border-0 pl-2 outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        className="outline-1 text-[12px] rounded-sm w-full border-0 pl-2 outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        <div className="w-full flex justify-end mb-[-12px]">
+                            <p className="text-xs text-subtext3">
+                                {formData.email.length}/70
+                            </p>
+                        </div>
                     </div>
                     <div>
-                        <p className="mb-1.5">Password</p>
+                        <p className="mb-1.5 text-sm font-medium">Password</p>
                         <input type="password"  
-                        onChange={(e) => setFormData(prev => ({...prev, password: e.target.value}))}
+                        onChange={(e) => Util.setValueLim((v: string) => setFormData(prev => ({...prev, password: v})), e.target.value, 40)}
                         value={formData.password}
                         placeholder={login ? "Enter Password" : "Create password"}
                         autoComplete="new-password"
-                        className="outline-1 text-[12px] rounded-xl w-full border-0 pl-2 outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        className="outline-1 text-[12px] rounded-sm w-full border-0 pl-2 outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        <div className={`w-full flex justify-end ${login ? "mb-[-5px]" : "mb-[-12px]"}`}>
+                            <p className="text-xs text-subtext3">
+                                {formData.password.length}/40
+                            </p>
+                        </div>
                     </div>
                     {!login ? <div>
-                        <p className="mb-1.5">Confirm Password</p>
+                        <p className="mb-1.5 text-sm font-medium">Confirm Password</p>
                         <input type="password"  
-                        onChange={(e) => setFormData(prev => ({...prev, confirmPassword: e.target.value}))}
+                        onChange={(e) => Util.setValueLim((v: string) => setFormData(prev => ({...prev, confirmPassword: v})), e.target.value, 40)}
                         value={formData.confirmPassword}
                         placeholder="Confirm password"
                         autoComplete="new-password"
-                        className="outline-1 text-[12px] rounded-xl w-full border-0 pl-2 outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        className="outline-1 text-[12px] rounded-sm w-full border-0 pl-2 outline-border2 text-sm p-1.5 text-subtext1 mb-1"/>
+                        <div className={`w-full flex justify-end mb-[-5px]`}>
+                            <p className="text-xs text-subtext3">
+                                {formData.confirmPassword.length}/40
+                            </p>
+                        </div>
                     </div>: ""}
-                    <button className="flex justify-center bg-btn rounded-xl p-1 text-btn-text outline-1 outline-border2 darkmode:outline-0 mt-1.5 hover:cursor-pointer  duration-400 ease-in-out  h-8 items-center"
+                    <button className="flex justify-center gap-2 bg-btn rounded-sm p-1 text-btn-text font-medium text-sm  outline-border2 darkmode:outline-0 mt-1.5 hover:cursor-pointer  duration-400 ease-in-out  h-8 items-center"
                     type="submit">
-                        {auth.loading ? <AiOutlineLoading className="animate-spin" /> : (login? "Log In" : "Sign Up")}
+                         {auth.loading ? <AiOutlineLoading className="animate-spin" /> : (login? "Log In"  : "Sign Up")} {auth.loading ? "" : <LuLogIn />}
                     </button>
                     <div className="flex justify-center mt-1.5 mb-8">
                         <p className="text-[13px] text-subtext3 text-center">
                             {login ? "Don't have and acount?" : "Allready have an acount?"}
                             <a className="text-highlight hover:cursor-pointer" 
                                 onClick={() => setLogin(!login)}> 
-                                {(login ? " Sign Up" : " Log In")}
+                                 {(login ? " Sign Up" : " Log In")}
                             </a>
                         </p>
                     </div>
