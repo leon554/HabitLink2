3b8b22a90f7cb366f365edadd5c0577b6afe06cf
Leon <codetips4572@gmail.com>
2025-09-05 16:02:23 +1000

    Added drop down animation

--- src/components/InputComponents/Select.tsx
+++ src/components/InputComponents/Select.tsx
@@ -60,7 +60,7 @@ export default function Select(props: SelectProps) {
     return (
         <div className={`relative ${props.divStyles}`} ref={focusElement}>
             <button className={`group relative transition-transform flex items-center gap-1  hover:cursor-pointer ${props.style ? props.style : " outline-1 bg-stone-800 text-sm text-stone-300 font-mono p-3 rounded-md flex justify-center  "}`}
-                 onClick={(e) => {
+                onClick={(e) => {
                     triggerHaptic()
                     setClicked(!clicked)
                     props.onBtnClick?.()
@@ -69,7 +69,9 @@ export default function Select(props: SelectProps) {
                 {props.setText 
                     ?? (props.selectedItem == null 
                     ? props.defaultText 
-                    : Util.capitilizeFirst(props.selectedItem.name))} {props.showIcon ? <IoIosArrowDown className="mt-0.5"/> : null}
+                    : Util.capitilizeFirst(props.selectedItem.name))} {props.showIcon ? <IoIosArrowDown className={`mt-0.5 transition-transform duration-300 ${
+                        clicked? "rotate-[180deg]" : "rotate-0"
+                    }`}/> : null}
 
             </button>
             <div className={`bg-panel2 absolute ${props.dropUp ? "bottom-full" : "top-full"}  ${props.center ? "left-1/2 transform -translate-x-1/2" : "right-0 "} rounded-xl p-2.5  overflow-y-scroll no-scrollbar max-h-100 px-1.5 mt-2 mb-1 ${props.largeText ? "gap-1.5" : "gap-1"} flex flex-col justify-start items-start scale-0 transition-transform duration-200 bg-panel1 text-subtext1  outline-border2  z-20 w-fit outline-1`} style={{
